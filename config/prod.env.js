1b6456b4a1a0f4374b47444fc033b8e8dd205194
Peng <896000867@qq.com>
2019-06-21 08:42:30 +0800

    修改生产环境为线上环境

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.1.100:8083"', // 47.104.177.198 192.168.1.100
-  U3D_IP: '"http://192.168.1.100:9004/qnnc2c"',
-  URL_IP: '"http://192.168.1.100:8083"'
+  BASE_API: '"http://www.mktail.cn:8083"', // 47.104.177.198 192.168.1.100
+  U3D_IP: '"http://www.mktail.cn:9004/"',
+  URL_IP: '"http://www.mktail.cn:8083"'
 }
