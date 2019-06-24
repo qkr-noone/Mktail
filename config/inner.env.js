6426650cf6eb8c283d9d10849e20b5113675bf15
Peng <896000867@qq.com>
2019-06-24 11:31:25 +0800

    新增前台系统内测环境配置

---
+++ config/inner.env.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://192.168.1.100:8083"',
+  U3D_IP: '"http://192.168.1.100:9004/qnnc2c"',
+  BASE_AFTER: '"http://192.168.1.100:9002"'
+}
