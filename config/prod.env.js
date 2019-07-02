6f87eb6b7a063d6155145e53b8d349e95c780163
Peng <896000867@qq.com>
2019-07-02 15:34:37 +0800

    修改构建命令名称和生产地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://www.mktail.cn:8483"', // 47.104.177.198 192.168.1.100
+  BASE_API: '"https://back.portal.mktail.cn"', // 47.104.177.198 192.168.1.100
   U3D_IP: '"https://threed.portal.mktail.cn/"',
   BASE_AFTER: '"https://seller.mktail.cn"' // 后台系统
 }
