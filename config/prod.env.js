1ca0a97a3c938f092640deb3e05626b16ba62dcf
Peng <896000867@qq.com>
2019-07-03 19:34:20 +0800

    删除生产环境的3D地址的/

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"https://back.portal.mktail.cn"', // 47.104.177.198 192.168.1.100
-  U3D_IP: '"https://threed.portal.mktail.cn/"',
+  U3D_IP: '"https://threed.portal.mktail.cn"',
   BASE_AFTER: '"https://seller.mktail.cn"' // 后台系统
 }
